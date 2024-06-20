51400c30c8bab4767d5bb2dc1925702e739de717
mnunn2 <bonnie650@gmail.com>
2024-06-20 10:28:04 +0100

    gear spacing added

--- src/components/gbSelector.tsx
+++ src/components/gbSelector.tsx
@@ -5,7 +5,7 @@ import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 
 const gearData = getGearData();
-type Ratios = Array<[string, number, number, number?]>;
+type Ratios = Array<[string, number, number, number, number?]>;
 
 const calcInt = (main: number, lay: number, topDiv: number): number => {
   return parseFloat(((lay / main) * topDiv).toFixed(3));
@@ -37,44 +37,89 @@ export default function GbSelector() {
 }
 
 function IntRatioTable({ name }: { name: string }) {
-
+  // build the ratio table and calc internal ratios
   const gb = gearData.filter((g) => g.name === name)[0];
-  const topDiv = gb.main4 / gb.lay4;
+  let topDiv: number = 0;
+  const tmpTable: Ratios = [];
+  // if there is a fifth gear
+  if (gb.main5 && gb.lay5) {
+    topDiv = gb.main5 / gb.lay5;
+    tmpTable.push([
+      "fourth",
+      gb.main4,
+      gb.lay4,
+      calcInt(gb.main4, gb.lay4, topDiv),
+    ]);
+    tmpTable.push([
+      "top",
+      gb.main5,
+      gb.lay5,
+      calcInt(gb.main5, gb.lay5, topDiv),
+    ]);
+  } else {
+    topDiv = gb.main4 / gb.lay4;
+    tmpTable.push([
+      "top",
+      gb.main4,
+      gb.lay4,
+      calcInt(gb.main4, gb.lay4, topDiv),
+    ]);
+  }
 
   const ratiosTable: Ratios = [
     ["first", gb.main1, gb.lay1, calcInt(gb.main1, gb.lay1, topDiv)],
     ["second", gb.main2, gb.lay2, calcInt(gb.main2, gb.lay2, topDiv)],
     ["third", gb.main3, gb.lay3, calcInt(gb.main3, gb.lay3, topDiv)],
-    ["fourth", gb.main4, gb.lay4, calcInt(gb.main4, gb.lay4, topDiv)],
   ];
+
+  ratiosTable.push(...tmpTable);
+
+  // get the internal ratios column and calculate the gear spacing
+  let gearSpacing = "first - ";
+  let intRats = ratiosTable.map((v) => v[3]);
+  const overallSpacing = (intRats[0] - 1).toFixed(2);
+  // subtract each internal ratio from the previous one
+  let spaces = intRats.map((e, i) => {
+    return e - intRats[i + 1];
+  });
+
+  spaces.map((e, i) => {
+    if (i === 0 && !isNaN(e))
+      gearSpacing = gearSpacing + e.toFixed(1) + " - second - ";
+    if (i === 1 && !isNaN(e))
+      gearSpacing = gearSpacing + e.toFixed(1) + " - third - ";
+    if (i === 2 && !isNaN(e))
+      gearSpacing = gearSpacing + e.toFixed(1) + " - fourth";
+    if (i === 3 && !isNaN(e))
+      gearSpacing = gearSpacing + " - " + e.toFixed(1) + " - fifth";
+  });
 
   return (
     <>
-      <h2>{gb.name}</h2>
-      <div>
-        <Table striped bordered size="sm">
-          <thead>
-            <tr>
-              <th>Gear</th>
-              <th>Mainshaft</th>
-              <th>Layshaft</th>
-              <th>Internal Ratio</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ratiosTable.map((g, i) => {
-              return (
-                <tr key={i}>
-                  <td>{g[0]}</td>
-                  <td>{g[1]}</td>
-                  <td>{g[2]}</td>
-                  <td>{g[3]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
+      <Table striped bordered size="sm">
+        <thead>
+          <tr>
+            <th>Gear</th>
+            <th>Mainshaft</th>
+            <th>Layshaft</th>
+            <th>Internal Ratio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ratiosTable.map((g, i) => {
+            return (
+              <tr key={i}>
+                <td>{g[0]}</td>
+                <td>{g[1]}</td>
+                <td>{g[2]}</td>
+                <td>{g[3]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <p>{gearSpacing}</p>
+      <p>overall spacing: first - {overallSpacing} -  top</p>
     </>
   );
 }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -21,4 +21,7 @@ export const gearData: GearBoxs = [
   { id: 3, name: "b25-44", main1: 16, lay1: 27, main2: 21, lay2: 22, main3: 24, lay3: 19, main4: 22, lay4: 14, },
   { id: 4, name: "b series std", main1: 16, lay1: 27, main2: 20, lay2: 23, main3: 24, lay3: 19, main4: 26, lay4: 17, },
   { id: 5, name: "b series rrt2", main1: 19, lay1: 24, main2: 22, lay2: 21, main3: 24, lay3: 19, main4: 25, lay4: 18, },
+  { id: 6, name: "Triumph 4 speed pre 69", main1: 16, lay1: 30, main2: 20, lay2: 26, main3: 24, lay3: 22, main4: 26, lay4: 20, },
+  { id: 7, name: "Triumph 4 speed 69 on", main1: 16, lay1: 30, main2: 20, lay2: 26, main3: 23, lay3: 22, main4: 26, lay4: 20, },
+  { id: 8, name: "Triumph 5 speed", main1: 13, lay1: 24, main2: 16, lay2: 21, main3: 18, lay3: 18, main4: 20, lay4: 17, main5: 21, lay5: 15, }, 
 ];
