daa4742a05b5ddc401ba7984ab15bdde1c73cfef
mnunn2 <bonnie650@gmail.com>
2024-07-21 19:23:26 +0100

    fix dark mode issue 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    "Visualize British Motorcycle Gearbox Ratios and their effects on Road Speeds",
+    "BSA Norton and Triumph Motorcycle Gear Ratios and their effects on Road Speeds",
   description:
     "This page enables you to enter different gear ratios and sprocket sizes to visualize the effects on road speeds. You can select from BSA Triumph or Notron gearboxws or manually enter the ratios",
 };
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-bs-theme="light">
       <body className={inter.className}>{children}</body>
     </html>
   );
